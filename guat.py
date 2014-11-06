1d293461cb1b01f0cf0db9aa8b1050b1fbc9c445
Wojtechnology <wswiders@uwaterloo.ca>
2014-11-06 13:05:08 -0500

    Separate Guatmobile

---
+++ guat.py
@@ -0,0 +1,37 @@
+from myro import *
+from time import time
+
+# Robot class
+class Guatmobile:
+
+    # Logic Variables
+    done = 0
+    beginTime = 0
+    timeElapsed = 0
+
+    # Functions to calculate how long it took to turn at the beginning
+    def startTime(self):
+        self.beginTime = time()
+
+    def endTime(self):
+        self.timeElapsed = time() - self.beginTime
+        return self.timeElapsed
+
+    # Delaying functions
+    def checkBright(self):
+        while self.done == 0:
+            if (getLight(0) > 4000000):
+                self.startTime()
+                beep(1, 880)
+                beep(1, 880)
+                beep(1, 880)
+                self.checkTimer()
+        self.done = 0
+
+    # Delaying functions
+    def checkTimer(self):
+        while self.done == 0:
+            if (time() - self.beginTime >= 10):
+                beep(3, 880)
+                self.done = 1
+                self.endTime()
--- main.py
+++ main.py
@@ -1,44 +1,6 @@
 from myro import *
 from time import time
-
-init("/dev/tty.Fluke2-052F-Fluke2")
-
-# Robot class
-class Gautmobile:
-
-    # Logic Variables
-    done = 0
-    beginTime = 0
-    turntime = 0
-
-    # Functions to calculate how long it took to turn at the beginning
-    def startTime(self):
-        self.beginTime = time()
-
-    def endTime(self):
-        self.turnTime = time() - self.beginTime
-        return self.turnTime
-
-    # Delaying functions
-    def checkBright(self):
-        while self.done == 0:
-            if (getLight(0) > 3000):
-                startTime()
-                beep(1, 880)
-                beep(1, 880)
-                beep(1, 880)
-                self.done = 1
-                checkTimer()
-        self.done = 0
-
-    # Delaying functions
-    def checkTimer(self):
-        while self.done == 0:
-            if (time() - self.beginTime >= 120):
-                beep(3, 880)
-                self.done = 1
-                endTime()
-        self.done = 0
+from guat import Guatmobile
 
 # -------------------- Start of Run Sequence ----------------------
 
@@ -46,7 +8,7 @@ class Gautmobile:
 def run():
 
     # Initialize
-    robot = Gautmobile()
+    robot = Guatmobile()
 
     # Loop until the light turns on
     robot.checkBright()
@@ -56,16 +18,17 @@ def run():
 
 # User Control
 def main():
+    init("/dev/tty.Fluke2-052F-Fluke2")
     finished = 0
-
+    
     while finished == 0:
-    select = raw_input("Enter 'r' to run\nEnter 'q' to quit program\n")
+        select = raw_input("Enter 'r' to run\nEnter 'q' to quit program\n")
 
-    if select == 'r':
-        run()
+        if select == 'r':
+            run()
 
-    # Ends the program
-    elif select == 'q':
-        finished = 1
+        # Ends the program
+        elif select == 'q':
+            finished = 1
 
 main()
