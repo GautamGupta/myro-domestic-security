b43293b0b846800a31b9cbc3a61ea3448df4a0b7
Wojtechnology <wswiders@uwaterloo.ca>
2014-11-08 16:22:56 -0500

    New photo file

--- photo.py
+++ photo.py
@@ -1,7 +1,53 @@
 from myro import *
 
-init("/dev/tty.Fluke2-052F-Fluke2")
+class RobotPic:
 
-picture = takePicture("color")
+	picture = None
 
-savePicture(picture, "pic.jpg")
+	def picTake(self, type):
+		try:
+			self.picture = takePicture(type)
+
+		except IOError:
+			print "Could not take picture"
+
+		return self
+
+	def picOpen(self, fileName):
+		try:
+			self.picture = makePicture(fileName)
+
+		except IOError:
+			print "Could not find or open file of name", fileName
+
+		return self
+
+	def picSave(self, fileName):
+		if(not self.picture):
+			print "No picture to be saved"
+			return self
+
+		try:
+			savePicture(self.picture, fileName)
+
+		except IOError:
+			print "Could not save file of name", fileName
+
+		return self
+
+	def picShow(self, name):
+		if(not self.picture):
+			print "No picture to be shown"
+			return self
+
+		show(self.picture, name)
+
+		return self
+
+def run():
+	robotPic = RobotPic()
+	robotPic.picOpen("pic.jpg")
+	robotPic.picShow("Picture")
+	return
+
+run()
